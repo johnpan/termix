44cb3b1cb19c5e09b69213b0d722e5ba48a21d4b
johnpan <johnnakis@gmail.com>
2019-09-16 15:38:30 +0300

    Bat experiments

--- merges.bat
+++ merges.bat
@@ -6,9 +6,15 @@ if "%_gitOK%" neq "nothing to commit, working tree clean" (
     echo ______ Working tree is NOT clean, please commit changes
     goto :eof
 )
+echo __pull
 git pull
-echo ______ merge develop into master and push master
+echo __push
+git push
+echo __checkout master
 git checkout master
+echo __merge develop
 git merge develop
+echo __push
 git push
 echo ______ Ready
+
